08db570650b50e3bfb759cd2f76587c5d76f6be1
Tadeu Jr <tadeujr@gmail.com>
2024-06-20 00:36:15 -0300

    organização do settings conforme django 5

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,8 @@ python manage.py makemigrations
 python manage.py migrate
 echo "from django.contrib.auth.models import User;
 from django.contrib.auth.hashers import make_password;
-User.objects.create(is_staff=True,is_superuser=True, username='$DJANGO_SUPERUSER_USERNAME', email='$DJANGO_SUPERUSER_EMAIL', password=make_password('$DJANGO_SUPERUSER_PASSWORD'))" | python manage.py shell
+if not User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists():
+    User.objects.create(is_staff=True, is_superuser=True, username='$DJANGO_SUPERUSER_USERNAME', email='$DJANGO_SUPERUSER_EMAIL', password=make_password('$DJANGO_SUPERUSER_PASSWORD'))" | python manage.py shell
+
 # Start the Django server
 exec "$@"
