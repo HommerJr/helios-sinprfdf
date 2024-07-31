dc1df6bc8be19342d98871b3f0f32287c347f47b
Tadeu Jr <tadeujr@gmail.com>
2024-07-31 11:12:15 -0400

    implementadas modificações de vndmtrx/helios-server até 31/7/24

--- heliosbooth/verifier.js
+++ heliosbooth/verifier.js
@@ -4,54 +4,54 @@
 function verify_ballot(election_raw_json, encrypted_vote_json, status_cb) {
     var overall_result = true;
     try {
-	election = HELIOS.Election.fromJSONString(election_raw_json);
-	var election_hash = election.get_hash();
-	status_cb("Fingerprint da eleição é " + election_hash);
-	
-	// display ballot fingerprint
-	encrypted_vote = HELIOS.EncryptedVote.fromJSONObject(encrypted_vote_json, election);
-	status_cb("Rastreador da cédula é " + encrypted_vote.get_hash());
-	
-      // check the hash
-      if (election_hash == encrypted_vote.election_hash) {
-          status_cb("Fingerprint da eleição confere com a cédula");
-      } else {
-          overall_result = false;
-          status_cb("PROBLEMA = fingerprint da eleição não confere com a cédula");          
-      }
-      
-      // display the ballot as it is claimed to be
-      status_cb("Conteúdo da Cédula:");
-      _(election.questions).each(function(q, qnum) {
-	      if (q.tally_type != "homomorphic") {
-		  status_cb("AVISO: o tipo de contagem para esta questão não é homomórfico. A verificação pode falhar porque este verificador está configurado apenas para lidar com cédulas homomórficas.");
-	      }
-        
-	      var answer_pretty_list = _(encrypted_vote.encrypted_answers[qnum].answer).map(function(aindex, anum) {
-		      return q.answers[aindex];
-		  });
-	      status_cb("Pergunta #" + (qnum+1) + " - " + q.short_name + " : " + answer_pretty_list.join(", "));
-      });
-      
-      // verify the encryption
-      if (encrypted_vote.verifyEncryption(election.questions, election.public_key)) {
-          status_cb("Criptografia Verificada");
-      } else {
-          overall_result = false;
-          status_cb("PROBLEMA = A criptografia não corresponde.");
-      }
-      
-      // verify the proofs
-      if (encrypted_vote.verifyProofs(election.public_key, function(ea_num, choice_num, result) {
-      })) {
-          status_cb("Conferência Realizada");
-      } else {
-          overall_result = false;
-          status_cb("PROBLEMA = As provas não validam.");
-      }
+        election = HELIOS.Election.fromJSONString(election_raw_json);
+        var election_hash = election.get_hash();
+        status_cb("O identificador da eleição é " + election_hash);
+
+        // display ballot fingerprint
+        encrypted_vote = HELIOS.EncryptedVote.fromJSONObject(encrypted_vote_json, election);
+        status_cb("Rastreador da cédula é " + encrypted_vote.get_hash());
+
+        // check the hash
+        if (election_hash == encrypted_vote.election_hash) {
+            status_cb("A identificação da eleição confere com a cédula");
+        } else {
+            overall_result = false;
+            status_cb("PROBLEMA = a identificação da eleição não confere com a cédula");
+        }
+
+        // display the ballot as it is claimed to be
+        status_cb("Conteúdo da Cédula:");
+        _(election.questions).each(function (q, qnum) {
+            if (q.tally_type != "homomorphic") {
+                status_cb("AVISO: o tipo de contagem para esta questão não é homomórfico. A verificação pode falhar porque este verificador está configurado apenas para lidar com cédulas homomórficas.");
+            }
+
+            var answer_pretty_list = _(encrypted_vote.encrypted_answers[qnum].answer).map(function (aindex, anum) {
+                return q.answers[aindex];
+            });
+            status_cb("Pergunta #" + (qnum + 1) + " - " + q.short_name + " : " + answer_pretty_list.join(", "));
+        });
+
+        // verify the encryption
+        if (encrypted_vote.verifyEncryption(election.questions, election.public_key)) {
+            status_cb("Criptografia Verificada");
+        } else {
+            overall_result = false;
+            status_cb("PROBLEMA = A criptografia não corresponde.");
+        }
+
+        // verify the proofs
+        if (encrypted_vote.verifyProofs(election.public_key, function (ea_num, choice_num, result) {
+        })) {
+            status_cb("Conferência Realizada");
+        } else {
+            overall_result = false;
+            status_cb("PROBLEMA = As provas não validam.");
+        }
     } catch (e) {
-      status_cb('Problema na análise de estruturas de dados eleitorais ou eleitorais, entradas malformadas:' + e.toString());
-      overall_result=false;
+        status_cb('Problema na análise de estruturas de dados eleitorais ou eleitorais, entradas malformadas:' + e.toString());
+        overall_result = false;
     }
 
     return overall_result;
